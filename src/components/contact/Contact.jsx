06aa1f78c380c158850f41c0029d98e5cd3d3296
Thallia Borba Rosa Salbego <thalliaborbarosa@gmail.com>
2022-02-08 13:59:29 +0000

    added email js to contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 import "./contact.css"
 import { MdPhone } from "react-icons/md";
 import { MdLocationOn } from "react-icons/md";
 import { MdEmail } from "react-icons/md";
+import emailjs from "@emailjs/browser";
+
 
 const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+        emailjs.sendForm('service_zjcyu8b', 'template_tjdq48k', formRef.current, 'user_9jYxriTNGk40827j7HhwV')
+      .then((result) => {
+          console.log(result.text);
+          setDone(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
     <div className="c">
       <div className="c-bg"></div>
@@ -33,12 +50,13 @@ const Contact = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt,
             natus?
           </p>
-          <form>
+          <form ref={formRef} onSubmit={handleSubmit}>
             <input type="text" placeholder="Name" name="user_name" />
             <input type="text" placeholder="Subject" name="user_subject" />
             <input type="text" placeholder="Email" name="user_email" />
             <textarea rows="5" placeholder='Message' name="message" />
             <button>Submit</button>
+            {done && "Thank you!"}
           </form>
         </div>
       </div>
